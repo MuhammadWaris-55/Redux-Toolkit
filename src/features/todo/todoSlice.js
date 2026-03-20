915a9b2d7ade41566d82d3c1ecc088f4b4a7f449
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-03-21 00:23:39 +0500

    Fix Reducers

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
-    todos: [{id: 1 , text: "Using Redux"}]
+    todos: [{ id: 1, text: "Using Redux" }]
 }
 
 export const todoSlice = createSlice({
@@ -20,7 +20,7 @@ export const todoSlice = createSlice({
             state.todos = state.todos.filter((todo) => todo.id !== action.payload)
         },
 
-          updateTodo: (state, action) => {
+        updateTodo: (state, action) => {
             const { id, text } = action.payload;
 
             const todo = state.todos.find((todo) => todo.id === id);
@@ -33,6 +33,6 @@ export const todoSlice = createSlice({
     }
 })
 
-export const {addTodo, removeTodo, updateTodo} = todoSlice.actions
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions
 
 export default todoSlice.reducer
