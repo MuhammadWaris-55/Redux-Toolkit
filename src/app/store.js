e659951862db1063744d58e4dd8d2e0ab51f144b
MuhammadWaris-55 <bhattimuhammadwaris@gmail.com>
2026-04-11 16:34:06 +0500

    Add Comments

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit'
 import todoReducer from '../features/todo/todoSlice'
 
+// Configure Redux store with the todo reducer
 export const store = configureStore({
     reducer: todoReducer
 })
